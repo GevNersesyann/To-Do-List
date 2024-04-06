944d87e468b3470ee3acb8901f931b9f06821b56
GevNersesyann <160640649+GevNersesyann@users.noreply.github.com>
2024-04-06 15:02:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("add").addEventListener("click", addToDo);
+
+function addToDo() {
+  const ul = document.getElementById("todo-list");
+  const inputText = document.getElementById("todo-input").value.trim();
+
+  if (inputText === "") {
+    alert("Please enter a valid task!");
+    return;
+  }
+
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const removeButton = document.createElement("button");
+
+  p.textContent = inputText;
+
+  li.appendChild(p);
+  li.appendChild(removeButton);
+  ul.appendChild(li);
+
+  removeButton.textContent = "Remove";
+  removeButton.addEventListener("click", function () {
+    ul.removeChild(li);
+  });
+
+  li.addEventListener("click", function () {
+    p.classList.toggle("completed");
+  });
+
+  document.getElementById("todo-input").value = "";
+}
